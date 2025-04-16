eb14a9eaf7610704422ca5179479632cc3b71a4a
Lucas Soria <soria.lucas@hotmail.com>
2025-04-15 21:08:12 -0300

    + Organización y format de URLs y Views.

--- src/autores/urls.py
+++ src/autores/urls.py
@@ -14,23 +14,30 @@ from .views import (
 app_name = 'app_autores'
 
 urlpatterns = [
+    # Registro
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    
+    # URLs Generales
     path('', views.inicio, name='base'),
     path('presentacion/', views.presentacion, name='presentacion'),
+    path('autores/json/', views.autores_json, name='autores_json'),
+    
+    # URLs de Autores
     path('autores/', views.autores, name='autores'),
     path('autores/crear_autor/', AutorCreateViews.as_view(), name='crear_autor'),
+    path('autores/<int:autor_id>/', DetalleAutorView.as_view(), name='detalle_autor'),
     path('autores/<int:pk>/borrar/', AutorDeleteView.as_view(), name='borrar_autor'),
+    path('autores/<int:pk>/modificar_autor/', AutorUpdateView.as_view(), name='modificar_autor'),
+    path('autores/<int:autor_id>/cambiar_estado/', views.cambiar_estado_autor, name='cambiar_estado_autor'),
+
+    # Filtro autores
+    path('autores/activos/', views.autores_activos, name='autores_activos'),
+    path('autores/inactivos/', views.autores_inactivos, name='autores_inactivos'),
+
+    # URLs de Frases
     path('frases/', views.frases_aleatorias, name='frases'),
     path('frases/crear_frase/', FraseCreateViews.as_view(), name='crear_frase'),
     path('autores/<int:autor_id>/frases/', views.frases_por_autor, name='frases_autor'),
     path('autores/<int:autor_id>/crear_frase/', FraseAutorCreateView.as_view(), name='crear_frase_autor'),
-    path('autores/<int:pk>/modificar_autor/', AutorUpdateView.as_view(), name='modificar_autor'),
-    path('autores/activos/', views.autores_activos, name='autores_activos'),
-    path('autores/inactivos/', views.autores_inactivos, name='autores_inactivos'),
-    path('autores/json/', views.autores_json, name='autores_json'),
-    #path('autores/<int:autor_id>/', views.detalle_autor, name='detalle_autor'),
-    path('autor/<int:autor_id>/', DetalleAutorView.as_view(), name='detalle_autor'),
-    path('autores/<int:autor_id>/borrar/', views.borrar_autor, name='borrar_autor'),
-    path('autores/<int:autor_id>/cambiar_estado/', views.cambiar_estado_autor, name='cambiar_estado_autor'),
 ]
--- src/autores/views.py
+++ src/autores/views.py
@@ -12,12 +12,15 @@ import random
 
 # Create your views here.
 
+
 @login_not_required
 def inicio(request):
     return render(request, 'base.html')
 
+
 def presentacion(request):
     return render(request, 'presentacion.html')
+
 
 @login_not_required
 def autores(request):
@@ -25,35 +28,19 @@ def autores(request):
     for autor in autores:
         # Se realizar el slogify y poder mostrar las imagenes
         autor.slug = slugify(autor.nombre)
-        # Nota: Esto no guarda el slug en la base de datos, solo lo calcula temporalmente para usarlo en la plantilla.
+        # Nota: Esto no guarda el slug en la base de datos, solo lo calcula temporalmente para usarlo en el template.
     return render(request, 'app_autores/autores.html', {'autores': autores})
+
 
 def autores_activos(request):
     autores = Autor.objects.filter(activo=True)
     return render(request, 'app_autores/activos.html', {'autores': autores})
 
+
 def autores_inactivos(request):
     autores = Autor.objects.filter(activo=False)
     return render(request, 'app_autores/inactivos.html', {'autores': autores})
 
-# Listado Json
-#def autores_json(request):
-#    autores = Autor.objects.all().values('id', 'nombre', 'nacionalidad', 'fecha_nacimiento', 'fecha_fallecimiento', 'activo')
-#    return JsonResponse(list(autores), safe=False)
-
-def autores_json(request):
-    autores = get_list_or_404(Autor)
-    autores_json = serializers.serialize('json', autores)
-    return JsonResponse(autores_json, safe=False)
-
-def detalle_autor(request, autor_id):
-    autor = get_object_or_404(Autor, id=autor_id)
-    return render(request, 'app_autores/detalle.html', {'autor': autor})
-
-def borrar_autor(request, autor_id):
-    autor = get_object_or_404(Autor, id=autor_id)
-    autor.delete()
-    return HttpResponseRedirect(reversed('app_autores:autores'))
 
 def cambiar_estado_autor(request, autor_id):
     autor = get_object_or_404(Autor, id=autor_id)
@@ -61,87 +48,34 @@ def cambiar_estado_autor(request, autor_id):
     autor.save()
     return HttpResponseRedirect(reverse('app_autores:autores'))
 
-@login_not_required
-def frases_aleatorias(request):
-    frases = list(Frase.objects.select_related('autor'))
-    random.shuffle(frases)  # Mezclamos las frases
-    for f in frases:
-        f.autor.slug = slugify(f.autor.nombre)
 
-    return render(request, 'app_autores/frases.html', {'frases': frases})
+def autores_json(request):
+    autores = get_list_or_404(Autor)
+    autores_json = serializers.serialize('json', autores)
+    return JsonResponse(autores_json, safe=False)
 
-def frases_por_autor(request, autor_id):
-    autor = get_object_or_404(Autor, id=autor_id)
-    frases = autor.frases.all()
-    # Para poder visualizar las IMG
-    autor.slug = slugify(autor.nombre)
-    
-    return render(request, 'app_autores/frases_autor.html', {'autor': autor, 'frases': frases})
 
-# Clases de Vistas
-#class FraseListViews(ListView):
-#    # model = Frase # Se puede solicitar tanto model como query
-#    queryset = Frase.objects.all()
-#    template_name = 'app_autores/frases.html'
-#    context_object_name = 'lista_frases' # Hace referecia a la relación del template
+# Listado Json
+#def autores_json(request):
+#    autores = Autor.objects.all().values('id', 'nombre', 'nacionalidad', 'fecha_nacimiento', 'fecha_fallecimiento', 'activo')
+#    return JsonResponse(list(autores), safe=False)
 
-class FraseCreateViews(CreateView):
-    model = Frase
-    fields = ['autor', 'texto']
-    success_url = reverse_lazy('app_autores:frases')
-    template_name = 'crear.html'
+#def detalle_autor(request, autor_id):
+#    autor = get_object_or_404(Autor, id=autor_id)
+#    return render(request, 'app_autores/detalle.html', {'autor': autor})
+
+#def borrar_autor(request, autor_id):
+#    autor = get_object_or_404(Autor, id=autor_id)
+#    autor.delete()
+#    return HttpResponseRedirect(reversed('app_autores:autores'))
 
 
 class AutorCreateViews(CreateView):
     model = Autor
-    fields = ['nombre', 'nacionalidad', 'fecha_nacimiento', 'fecha_fallecimiento', 'activo']
+    fields = ['nombre', 'nacionalidad', 'fecha_nacimiento',
+              'fecha_fallecimiento', 'activo']
     success_url = reverse_lazy('app_autores:autores')
     template_name = 'crear.html'
-
-# Realizado con GPT
-class FraseAutorCreateView(CreateView):
-    """
-    Vista para crear una frase asociada a un autor específico.
-    - Recibe el `autor_id` por URL.
-    - Asigna automáticamente el autor a la frase antes de guardarla.
-    - Redirige a la lista de frases del autor después de guardar.
-    """
-    
-    model = Frase
-    fields = ['texto']  # No se pide el autor, se fija en `form_valid`
-    template_name = 'app_autores/crear_frase_por_autor.html'
-
-    # Valida el formulario para el autor de a cuerdo al pk=ID
-    def form_valid(self, form):
-        autor_id = self.kwargs['autor_id']
-        form.instance.autor = get_object_or_404(Autor, pk=autor_id)
-        return super().form_valid(form)
-
-    # Redirecciona luego de guardar con exito
-    def get_success_url(self):
-        return reverse_lazy('app_autores:frases_autor', kwargs={'autor_id': self.kwargs['autor_id']})
-    
-    # Agrega datos al contexto, se agrega el autor (usando su ID) para poder mostrarlo como referencia visual (ejemplo en el titulo)
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['autor'] = Autor.objects.get(pk=self.kwargs['autor_id'])
-        return context
-    
-
-class AutorUpdateView(UpdateView):
-    model = Autor
-    fields = '__all__'
-    template_name = 'crear.html'
-    
-    # Regresa al detalle
-    def get_success_url(self):
-        return reverse_lazy('app_autores:detalle_autor', kwargs={'autor_id': self.object.id})
-
-
-class AutorDeleteView(DeleteView):
-    model = Autor
-    template_name = 'app_autores/borrar_autor.html'
-    success_url = reverse_lazy('app_autores:autores')
 
 
 class DetalleAutorView(DetailView):
@@ -155,4 +89,81 @@ class DetalleAutorView(DetailView):
         context = super().get_context_data(**kwargs)
         autor = context.get('autor')
         autor.slug = slugify(autor.nombre)
+        return context
+
+
+class AutorUpdateView(UpdateView):
+    model = Autor
+    fields = '__all__'
+    template_name = 'crear.html'
+
+    # Regresa al detalle
+    def get_success_url(self):
+        return reverse_lazy('app_autores:detalle_autor', kwargs={'autor_id': self.object.id})
+
+
+class AutorDeleteView(DeleteView):
+    model = Autor
+    template_name = 'app_autores/borrar_autor.html'
+    success_url = reverse_lazy('app_autores:autores')
+
+
+@login_not_required
+def frases_aleatorias(request):
+    frases = list(Frase.objects.select_related('autor'))
+    random.shuffle(frases)  # Mezclamos las frases
+    for f in frases:
+        f.autor.slug = slugify(f.autor.nombre)  # -> Para poder ver las IMG
+
+    return render(request, 'app_autores/frases.html', {'frases': frases})
+
+
+def frases_por_autor(request, autor_id):
+    autor = get_object_or_404(Autor, id=autor_id)
+    frases = autor.frases.all()
+    autor.slug = slugify(autor.nombre)  # -> Para poder ver las IMG
+
+    return render(request, 'app_autores/frases_autor.html', {'autor': autor, 'frases': frases})
+
+# class FraseListViews(ListView):
+#    # model = Frase # Se puede solicitar tanto model como query
+#    queryset = Frase.objects.all()
+#    template_name = 'app_autores/frases.html'
+#    context_object_name = 'lista_frases' # Hace referecia a la relación del template
+
+
+class FraseCreateViews(CreateView):
+    model = Frase
+    fields = ['autor', 'texto']
+    success_url = reverse_lazy('app_autores:frases')
+    template_name = 'crear.html'
+
+
+# Realizado con GPT - Crea frases por autor
+class FraseAutorCreateView(CreateView):
+    """
+    Vista para crear una frase asociada a un autor específico.
+    - Recibe el `autor_id` por URL.
+    - Asigna automáticamente el autor a la frase antes de guardarla.
+    - Redirige a la lista de frases del autor después de guardar.
+    """
+
+    model = Frase
+    fields = ['texto']  # No se pide el autor, se fija en `form_valid`
+    template_name = 'app_autores/crear_frase_por_autor.html'
+
+    # Valida el formulario para el autor de a cuerdo al pk=ID
+    def form_valid(self, form):
+        autor_id = self.kwargs['autor_id']
+        form.instance.autor = get_object_or_404(Autor, pk=autor_id)
+        return super().form_valid(form)
+
+    # Redirecciona luego de guardar con exito
+    def get_success_url(self):
+        return reverse_lazy('app_autores:frases_autor', kwargs={'autor_id': self.kwargs['autor_id']})
+
+    # Agrega datos al contexto, se agrega el autor (usando su ID) para poder mostrarlo como referencia visual (ejemplo en el titulo)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['autor'] = Autor.objects.get(pk=self.kwargs['autor_id'])
         return context
